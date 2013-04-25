99ece75035dd6febb4770c16933120575ba5652b
Scott Weston <scottw@gmail.com>
2013-04-25 20:50:41 +1000

    added atomizer config

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,6 +1,7 @@
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new( '/news' ) 
   extension Awestruct::Extensions::Paginator.new(:posts, '/news/index', :per_page => 5)
+  extension Awestruct::Extensions::Atomizer.new(:posts, '/news/atom.xml', :feed_title=>'dereel.com.au')
   extension Awestruct::Extensions::Disqus.new
   transformer Awestruct::Extensions::Minify.new([:js])
   helper Awestruct::Extensions::GoogleAnalytics
