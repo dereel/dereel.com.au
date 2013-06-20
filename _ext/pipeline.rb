829513c46ab7197d91e382633843d04e2990e8bf
Scott Weston <scottw@gmail.com>
2013-06-21 09:58:40 +1000

    update for awestruct 0.5.2 support

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,3 +1,5 @@
+require 'bootstrap-sass'
+
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new( '/news' ) 
   extension Awestruct::Extensions::Paginator.new(:posts, '/news/index', :per_page => 5)
